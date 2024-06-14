12e2dd210a3f4508bc0e4139b4e8d54eac7964fd
Rizky Subagja <rizkysubagja99@gmail.com>
2024-06-14 23:46:50 +0700

    handle validate email and phone number

--- services/OtpService.js
+++ services/OtpService.js
@@ -1,12 +1,25 @@
 const createError = require('http-errors')
+const validator = require('validator');
 
 class OtpService {
   constructor(options) {
     Object.assign(this, options);
   }
 
+  _validateEmail(email) {
+    return validator.isEmail(email);
+  }
+
   async initOtp(payload) {
+    const { email } = payload;
+
     try {
+      const isValidEmail = this._validateEmail(email);
+
+      if (!isValidEmail) {
+        throw createError(400, 'Email format is invalid', { code: 'INVALID_FORMAT' });
+      }
+
       const generatedOtp = Math.floor(Math.random() * 1000000);
 
       const otpPayload = {
--- services/UserService.js
+++ services/UserService.js
@@ -1,11 +1,33 @@
+const createError = require('http-errors')
+const validator = require('validator');
+
 class UserService {
   constructor(options) {
     Object.assign(this, options);
   }
 
+  _validatePhoneNumber(phoneNumber) {
+    return validator.isNumeric(phoneNumber);
+  }
+
+  _validateEmail(email) {
+    return validator.isEmail(email);
+  }
+
   async addAccount(payload) {
+    const { email, phoneNumber } = payload;
+
     try {
-      const createdUser = await this.userRepository.saveUser(payload);
+      const isValidEmail = this._validateEmail(email);
+      const isValidPhoneNumber = this._validatePhoneNumber(phoneNumber);
+
+      if (!isValidEmail || !isValidPhoneNumber) {
+        throw createError(400, 'Email or phone number format is invalid', { code: 'INVALID_FORMAT' });
+      }
+
+      const id = await this.userRepository.saveUser(payload);
+
+      const createdUser = await this.userRepository.getUserById(id);
 
       return createdUser;
     } catch (error) {
